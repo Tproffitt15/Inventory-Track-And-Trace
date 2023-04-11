1e38c362adfe9d2df7e474320394598f10727503
proffitz <proffitz@miamioh.edu>
2023-04-11 00:33:56 -0400

    polished navbar

--- src/components/HomepageNavbar.js
+++ src/components/HomepageNavbar.js
@@ -1,16 +1,27 @@
 import styles from "./HomepageNavbar.module.css"
+import cardinalLogo from "../assets/cardinal-health-logo.png"
+import profileLogo from "../assets/profile.PNG";
+import ordersLogo from "../assets/Orders.PNG";
 
 import { Link, Outlet } from "react-router-dom"
 
 
 const HomepageNavbar = () => {
     return (
-        <div className={styles.homepageNavbar}>
+        <div className={styles.HomepageNavbar}>
             <ul>
-                <li><Link to="create-order">Create Orders</Link></li>
-                {/* <li><a className="active" href="createNewOrder.html">Cardinal Health</a></li> */}
-                <li><Link to="orders">My Orders</Link></li>
-                <li><Link to="profile">Profile</Link></li>
+                {/* <img src="https://www.cardinalhealth.com/content/dam/corp/web/logos/logo-main.png" alt="Cardinal Health logo"></img> */}
+                <img src={cardinalLogo} alt="Cardinal Health logo" className={styles.cardinalLogo}></img>
+                {/* <li><Link to="create-order" className={styles.HomepageNavbarOption}>Create Orders</Link></li> */}
+                              
+                <li>
+                    <img src={ordersLogo} alt="orders logo" className={styles.icon}></img>
+                    <Link to="profile">Profile</Link>
+                </li>
+                <li>
+                    <img src={profileLogo} alt="profile logo" className={styles.icon}></img>
+                    <Link to="orders">Orders</Link>
+                </li>
             </ul>
             <Outlet />
         </div>
