51e38965fd7c251e9ed2f9b09484b11015a13095
Bach Tran <tranbn2@miamioh.edu>
2023-04-18 14:06:26 -0400

    save changes

--- src/pages/OrderDetails.js
+++ src/pages/OrderDetails.js
@@ -3,7 +3,7 @@ import Status from "components/Status";
 import OrderJSON from "../order.json";
 import React, { useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
-
+import partiesData from "data/parties.json"
 import OrderDetailsTable from "components/OrderDetailTable";
 import ShippingInfo from "./ShippingInfo";
 
@@ -22,6 +22,7 @@ const OrderDetails = () => {
     const manufactureAddress = selectedOrder[0].manufacturer;
     const distributorAddress = selectedOrder[0].distributor;
     const customerAddress = selectedOrder[0].customer;
+    const catchAllAddress = partiesData.filter((person) => person.role === 3)[0].address;
 
     const renderOrderDetailsTable = () => {
         switch (selectedOption) {
@@ -66,29 +67,29 @@ const OrderDetails = () => {
                 {/* Distributor see Manufacturer Shipped, which is an incoming order */}
                 {userRole === 1 && status === 0 && (
                     <div>
-                        <li onClick={() => transferOwner(manufactureAddress, 3)} className="orderDetailsNavbarOption">Decline</li>
-                        <li onClick={() => transferOwner()} className="orderDetailsNavbarOption">Receive</li>
+                        <li onClick={() => transferOwner(catchAllAddress, 3)} className="orderDetailsNavbarOption">Decline</li>
+                        <li onClick={() => transferOwner(distributorAddress, 1)} className="orderDetailsNavbarOption">Receive</li>
                     </div>
                 )}
                 {/* Distributor after received decide to cancel or forward order */}
                 {userRole === 1 && status === 1 && (
                     <div>
                         <li onClick={() => transferOwner()} className="orderDetailsNavbarOption">Cancel</li>
-                        <li onClick={() => transferOwner()} className="orderDetailsNavbarOption">Forward Order</li>
+                        <li onClick={() => transferOwner(distributorAddress, 2)} className="orderDetailsNavbarOption">Forward Order</li>
                     </div>
                 )}
                 {/* Customer see distributor forward choose to reject or complete */}
                 {userRole === 2 && status === 2 && (
                     <div>
-                        <li onClick={() => transferOwner()} className="orderDetailsNavbarOption">Reject</li>
-                        <li onClick={() => transferOwner()} className="orderDetailsNavbarOption">Complete</li>
+                        <li onClick={() => transferOwner(catchAllAddress, 4)} className="orderDetailsNavbarOption">Reject</li>
+                        <li onClick={() => transferOwner(customerAddress, 5)} className="orderDetailsNavbarOption">Complete</li>
                     </div>
                 )}
                 {/* Customer if reject will finish or return the order */}
                 {userRole === 2 && status === 4 && (
                     <div>
                         <li onClick={() => transferOwner()} className="orderDetailsNavbarOption">Finish</li>
-                        <li onClick={() => transferOwner()} className="orderDetailsNavbarOption">Return Order</li>
+                        <li onClick={() => transferOwner(manufactureAddress, 6)} className="orderDetailsNavbarOption">Return Order</li>
                     </div>
                 )}
             </div>
